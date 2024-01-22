92d6216200b0bf091e8e3ce8eecf2d8c193c0287
Deep03 <lekhakdeepak10@gmail.com>
2024-01-22 00:30:52 -0500

    completed Facenet model

--- inception_mdls.py
+++ inception_mdls.py
@@ -1,20 +1,33 @@
 from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import tensorflow as tf
 import tf_slim as slim
 import tensorflow as tf
-
 import keras
+from keras.layers import Conv2D, MaxPooling2D, concatenate, GlobalAveragePooling2D, Dense, BatchNormalization
 
 # Load InceptionV3 without the top classification layer
-def inception_net(input_tensor):
+def inception_v3(input_shape):
     inception_layer = keras.applications.InceptionV3(
         include_top=False,
-        weights='None',  # You can specify 'None' to start with random weights
-        input_tensor=input_tensor, # (n - 1) tensor
-        input_shape=(299, 299, 3),  # Adjust input shape based on your requirements,
+        weights=None,  # You can specify 'None' to start with random weights
+        input_shape=input_shape,  # Adjust input shape based on your requirements,
     )
 
     return inception_layer
+
+
+    
+def inception_module(x, filters_1x1, filters_3x3_reduce, filters_3x3, filters_5x5_reduce, filters_5x5, filters_pool_proj, name):
+    conv1x1 = Conv2D(filters_1x1, (1, 1), padding='same', activation='relu', name=name+'_1x1')(x)
+
+    conv3x3_reduce = Conv2D(filters_3x3_reduce, (1, 1), padding='same', activation='relu', name=name+'_3x3_reduce')(x)
+    conv3x3 = Conv2D(filters_3x3, (3, 3), padding='same', activation='relu', name=name+'_3x3')(conv3x3_reduce)
+
+    conv5x5_reduce = Conv2D(filters_5x5_reduce, (1, 1), padding='same', activation='relu', name=name+'_5x5_reduce')(x)
+    conv5x5 = Conv2D(filters_5x5, (5, 5), padding='same', activation='relu', name=name+'_5x5')(conv5x5_reduce)
+
+    pool_proj = MaxPooling2D((3, 3), strides=(1, 1), padding='same', name=name+'_pool')(x)
+    pool_proj = Conv2D(filters_pool_proj, (1, 1), padding='same', activation='relu', name=name+'_pool_proj')(pool_proj)
+
+    inception_module = concatenate([conv1x1, conv3x3, conv5x5, pool_proj], axis=-1, name=name+'_concat')
+
+    return inception_module
--- model.py
+++ model.py
@@ -1,40 +1,49 @@
-import tensorflow as tf
-import tf_slim as slim
-import keras
-from keras.layers import Input, Conv2D, MaxPooling2D, concatenate, GlobalAveragePooling2D, Dense, BatchNormalization
+from keras.layers import Input, Conv2D, MaxPooling2D, concatenate, AveragePooling2D, GlobalAveragePooling2D, Dense, BatchNormalization
 from keras.models import Model
+
 import inception_mdls
 
+# Input layer
+input_layer = Input(shape=(112, 112, 3), name='input_layer')
 
-def build_model():
-    input_layer = Input(shape=(112, 112, 3))  
+# Conv1
+conv1 = Conv2D(64, (7, 7), strides=(2, 2), padding='same', activation='relu', name='conv1')(input_layer)
 
-    # Conv1 (7x7)
-    conv1 = Conv2D(64, (7, 7), strides=(2, 2), padding='same', activation='relu')(input_layer)
+# Max Pool + Norm
+max_pool_norm = MaxPooling2D((2, 2), strides=(1, 1), padding='same', name='max_pool_norm')(conv1)
 
-    # Max Pool + Norm (3x3)
-    pool_norm = MaxPooling2D((3, 3), strides=(2, 2), padding='same')(conv1)
+# Inception (2)
+inception_2 = inception_mdls.inception_module(max_pool_norm, 64, 64, 192, 1, 1, 1, name='inception_2')
 
-    # InceptionV3 Layer
-    inception_net = inception_mdls.inception_net(pool_norm)
 
-    # Avg Pool (1x1)
-    avg_pool = GlobalAveragePooling2D()(inception_net)
+# Norm + Max Pool
+norm_max_pool = MaxPooling2D((3, 3), strides=(2, 2), padding='same', name='norm_max_pool')(inception_2)
 
-    # Fully Conn (1x1x128)
-    fully_conn = Dense(128, activation='relu')(avg_pool)
 
-    # L2 Normalization (1x1x128)
-    l2_norm = BatchNormalization()(fully_conn)
+# Inception Layer taken form https://arxiv.org/pdf/1503.03832.pdf & https://arxiv.org/pdf/1409.4842.pdf
+inception_3a = inception_mdls.inception_module(norm_max_pool, 64, 96, 128, 16, 32, 32, name='inception_3a')
+inception_3b = inception_mdls.inception_module(inception_3a, 64, 96, 128, 32, 64, 64, name='inception_3b')
+inception_3c = inception_mdls.inception_module(inception_3b, 128, 128, 256, 32, 64, 64, name='inception_3c')
 
-    # Output
-    output_layer = Dense(1, activation='relu')(l2_norm)
+inception_4a = inception_mdls.inception_module(inception_3c, 256, 96, 192, 32, 64, 128, name='inception_4a')
+inception_4b = inception_mdls.inception_module(inception_4a, 224, 112, 224, 32, 64, 128, name='inception_4b')
+inception_4c = inception_mdls.inception_module(inception_4b, 192, 128, 256, 32, 64, 128, name='inception_4c')
+inception_4d = inception_mdls.inception_module(inception_4c, 160, 144, 288, 32, 64, 128, name='inception_4d')
+inception_4e = inception_mdls.inception_module(inception_4d, 160, 160, 256, 64, 128, 128, name='inception_4e')
 
-    model = Model(inputs=input_layer, outputs=output_layer)
-    return model
 
-# Build the model
-model = build_model()
+inception_5a = inception_mdls.inception_module(inception_4e, 384, 192, 384, 48, 128, 128, name='inception_5a')
+inception_5b = inception_mdls.inception_module(inception_5a, 384, 192, 384, 48, 128, 128, name='inception_5b')
+
+avgpool = AveragePooling2D()(inception_5b)
+
+fully_conn = Dense(128, activation='relu', name='fully_conn')(avgpool)
+
+global_avg_pool = GlobalAveragePooling2D()(fully_conn)
+
+l2_norm = BatchNormalization()(global_avg_pool)
+
+model = Model(inputs=input_layer, outputs=l2_norm)
 
 # Print model summary
 model.summary()
